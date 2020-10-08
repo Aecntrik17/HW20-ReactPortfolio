4457524dbe7c946ef8c96452433e515a131a1489
Aaron Sims <aaron@adsimsllc.com>
2020-10-08 17:51:29 -0400

    update project.js file with props

--- reactportolio/src/pages/Portfolio.js
+++ reactportolio/src/pages/Portfolio.js
@@ -1,20 +1,21 @@
 import React from "react";
 import Project from "../components/Project/Project";
-import ProjectsArray from "../projects.json";
+import ProjectsArray from "../projects";
 
 function Portfolio() {
   return (
-    <container>
+    <div>
       {ProjectsArray.map((project) => (
         <Project
           id={project.id}
+          key={project.id}
           image={project.image}
           title={project.title}
           github={project.github}
           deployed={project.deployed}
         />
       ))}
-    </container>
+    </div>
   );
 }
 
