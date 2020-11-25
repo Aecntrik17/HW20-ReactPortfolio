84cb28cbbf5d74e14119e191317e3036106b4d5d
Aaron Sims <aaron@adsimsllc.com>
2020-11-25 14:51:48 -0500

    updating navbar and footer to ensure hamburger activates

--- reactportolio/src/components/Footer/index.js
+++ reactportolio/src/components/Footer/index.js
@@ -1,28 +1,49 @@
 import React from "react";
+import resume from "../../assets/resume.pdf";
 
 function Footer() {
   return (
-    <div className="footer">
-      <li className="nav-item">
-        <a
-          className="nav-link"
-          href="https://www.linkedin.com/in/aarondsims"
-          target="_blank"
-        >
-          LinkedIn
-        </a>
-      </li>
-
-      <li className="nav-item">
-        <a
-          className="nav-link"
-          href="https://www.github.com/Aecntrik17"
-          target="_blank"
-        >
-          Github
-        </a>
-      </li>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <span className="navbar-brand mb-1 h1"></span>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="nav navbar-nav navbar-right">
+          <li className="nav-item">
+            <a className="nav-link" href={resume} target="_blank">
+              Resume
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              href="https://www.linkedin.com/in/aarondsims"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              href="https://github.com/Aecntrik17"
+              target="_blank"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
--- reactportolio/src/pages/About.js
+++ reactportolio/src/pages/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import profilePic from "../assets/AaronSims.jpg";
-import resume from "../assets/resume.pdf";
 
 // the html display for the about page
 function About() {
@@ -10,12 +9,27 @@ function About() {
         <div className="col-md-12">
           <h1>About Me</h1>
           <img alt="Aaron Sims" src={profilePic} />
-          <p>Content</p>
-          <div>
-            <a className="nav-link" href={resume} target="_blank">
-              Resume
-            </a>
-          </div>
+          <p className="content">
+            A natural leader with a demeanor equipped for cultivating
+            relationships, effective communication, and teamwork with a proven
+            track record for delivering solutions across internal and external
+            partners. Currently earning a full stack web development certificate
+            from the University of North Carolina at Chapel Hill.
+          </p>
+          <p className="content">
+            Seeking an entry-level Front-End Developer opportunity where I can
+            apply newly acquired HTML, Javascript, and REACT skills while
+            leveraging my 15 + years of corporate and entrepreneurial experience
+            within the Financial Services industry. I intend to add value by
+            providing unique perspectives on end user interaction.
+          </p>
+          <p className="content">
+            Computer science is a new venture for me yet a welcomed challenge. I
+            have been working since the age of 13 and financed my self through
+            college while also playing D-I athletics. I strive for perfection in
+            all things professional and personal yet realize that often times
+            merely excellence is attainable.
+          </p>
         </div>
       </div>
     </div>
