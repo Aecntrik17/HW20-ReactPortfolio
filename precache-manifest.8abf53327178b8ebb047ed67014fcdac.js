c38fc75398b624d0f912a5941a0d67947c85bb24
Aaron Sims <aaron@adsimsllc.com>
2020-12-02 18:45:41 -0500

    Updates

---
+++ precache-manifest.8abf53327178b8ebb047ed67014fcdac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00dfce78672e062168c155c1e03433a",
+    "url": "/HW20-ReactPortfolio/index.html"
+  },
+  {
+    "revision": "89893f4389876ffd41f5",
+    "url": "/HW20-ReactPortfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "9e45789ec02e95ec16ff",
+    "url": "/HW20-ReactPortfolio/static/css/main.560411b6.chunk.css"
+  },
+  {
+    "revision": "89893f4389876ffd41f5",
+    "url": "/HW20-ReactPortfolio/static/js/2.7dd69d66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HW20-ReactPortfolio/static/js/2.7dd69d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e45789ec02e95ec16ff",
+    "url": "/HW20-ReactPortfolio/static/js/main.439ea276.chunk.js"
+  },
+  {
+    "revision": "8a3228f24637f4789e64",
+    "url": "/HW20-ReactPortfolio/static/js/runtime-main.c7720ffe.js"
+  },
+  {
+    "revision": "f3ac68ace2dd3bfbbcd7d6db51b1b53b",
+    "url": "/HW20-ReactPortfolio/static/media/Movin_on_up.f3ac68ac.gif"
+  },
+  {
+    "revision": "eaa8ef98c6ccb400ee92c98d38ddb3be",
+    "url": "/HW20-ReactPortfolio/static/media/day_planner.eaa8ef98.png"
+  },
+  {
+    "revision": "76bfb718ea9b83d479c9a42ce56c1c88",
+    "url": "/HW20-ReactPortfolio/static/media/javascript_quiz.76bfb718.png"
+  },
+  {
+    "revision": "e33a810908feb573c3ce62701e7657ac",
+    "url": "/HW20-ReactPortfolio/static/media/password_generator.e33a8109.png"
+  },
+  {
+    "revision": "80ad4c7f8ca6062c55a49784b9106953",
+    "url": "/HW20-ReactPortfolio/static/media/resume.80ad4c7f.pdf"
+  },
+  {
+    "revision": "3593a0959e0a99fd17a3942a63db092d",
+    "url": "/HW20-ReactPortfolio/static/media/workoutTracker.3593a095.png"
+  }
+]);
