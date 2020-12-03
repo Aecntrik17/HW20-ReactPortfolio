c453bbffbbbb64c9947c8d8e0c3b05fbd501c17a
Aaron Sims <aaron@adsimsllc.com>
2020-12-02 19:41:16 -0500

    updating CSS for portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,6 +2,7 @@ import React from "react";
 import Project from "../components/Project/Project";
 import ProjectsArray from "../projects";
 import Welcome from "../components/Welcome/Welcome";
+import "./portfolio.css";
 
 // will display the portfolio page of the project
 function Portfolio() {
@@ -10,22 +11,22 @@ function Portfolio() {
       <div>
         <Welcome />
       </div>
-      <div className="container">
-        <div className="project">
-          {/* looping through the projects array and pulling information based on the appropriate keys */}
-          {ProjectsArray.map((project) => (
-            // generating project component based on data in projects array
-            <Project
-              id={project.id}
-              key={project.id}
-              image={project.image}
-              title={project.title}
-              github={project.github}
-              deployed={project.deployed}
-            />
-          ))}
-        </div>
+      <div className="portfolioContainer">
+        {/* <div className="project"> */}
+        {/* looping through the projects array and pulling information based on the appropriate keys */}
+        {ProjectsArray.map((project) => (
+          // generating project component based on data in projects array
+          <Project
+            id={project.id}
+            key={project.id}
+            image={project.image}
+            title={project.title}
+            github={project.github}
+            deployed={project.deployed}
+          />
+        ))}
       </div>
+      {/* </div> */}
     </div>
   );
 }
