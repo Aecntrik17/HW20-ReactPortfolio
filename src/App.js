33dfd88188d1e526b63d15af9567d45f5182819d
Aaron Sims <aaron@adsimsllc.com>
2020-12-02 19:26:22 -0500

    someting serious with deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
@@ -12,24 +12,16 @@ import NoMatch from "./pages/NoMatch";
 // represents the main control for the html displays
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <div>
         <Nav />
         {/* allows for the differnt pages to be displayed when the corresponding routes via the URL*/}
         <Switch>
-          <Route exact path="/HW20-ReactPortfolio/" component={About} />
-          <Route exact path="/HW20-ReactPortfolio/About" component={About} />
-          <Route
-            exact
-            path="/HW20-ReactPortfolio/Portfolio"
-            component={Portfolio}
-          />
-          <Route
-            exact
-            path="/HW20-ReactPortfolio/Contact"
-            component={Contact}
-          />
-          <Route path="HW20-ReactPortfolio/" component={NoMatch} />
+          <Route exact path="/" component={About} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Portfolio" component={Portfolio} />
+          <Route exact path="/Contact" component={Contact} />
+          <Route path="/" component={NoMatch} />
         </Switch>
         {/* <Footer /> */}
       </div>
